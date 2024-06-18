2e15e7cfc57cb42e4b329e3369ddef8d64df334b
Hivenet-Nolan <144666332+Hivenet-Nolan@users.noreply.github.com>
2024-06-18 09:37:59 +0200

    Add update user mutation

--- src/resolvers/user/user.resolver.ts
+++ src/resolvers/user/user.resolver.ts
@@ -1,7 +1,7 @@
 import { firstValueFrom, timeout } from "rxjs";
 
 import { Inject } from "@nestjs/common";
-import { Args, Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { ClientProxy } from "@nestjs/microservices";
 
 import { User } from "./user.model";
@@ -24,6 +24,18 @@ export class UserResolver {
   async getUserProfile(@Args("userId") userId: string): Promise<User> {
     const data = await firstValueFrom(
       this.userServiceClient.send({ cmd: "getUserProfile" }, { userId }).pipe(timeout(5000))
+    );
+    return data;
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Args("userId") userId: string,
+    @Args("name") name: string,
+    @Args("email") email: string
+  ): Promise<User> {
+    const data = await firstValueFrom(
+      this.userServiceClient.send({ cmd: "updateUser" }, { userId, name, email }).pipe(timeout(5000))
     );
     return data;
   }
